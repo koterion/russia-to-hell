ffb4ea0da0ea6b18cf34a16ebaaaf3de833ac2f1
RGBsrs <grjanko.d.a@gmail.com>
2022-03-01 10:24:51 +0200

    remove request

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-import requests
 import os
 
 def myping(host):
@@ -9,24 +8,18 @@ def myping(host):
     else:
         return False
 
-url = 'https://raw.githubusercontent.com/koterion/russia-to-hell/36c6f4013c3be99743e948ee8ae5eec3778fa343/resources.txt'
-r = requests.get(url, allow_redirects=True)
-
 #open('resources.txt', 'wb').write(r.content)
 
-headers = {
-    'Accept': 'application/json'
-}
-base_url ='https://check-host.net/check-ping'
 live_urls = []
 
-content = r.content.decode('utf-8').split("\n")
-for line in content:
-    line = line.replace('https://', '')
-    line = line.replace('http://', '')
-    if line:
-        if myping(line):
-            live_urls.append(line)
+with open('resources.txt', 'r') as targets:
+    for line in targets.readlines():
+        line = line.replace('\n', '')
+        line = line.replace('https://', '')
+        line = line.replace('http://', '')
+        if line:
+            if myping(line):
+                live_urls.append(line)
 
 with open('activeHosts.txt', 'w') as f:
     for url in live_urls:
